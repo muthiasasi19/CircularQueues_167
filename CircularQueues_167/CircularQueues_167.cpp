31a3bbc5209179d92c8f2d64e2eeed87026d0ddb
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-06-23 06:23:48 +0200

    membuat switch case 1 dan 2

--- CircularQueues_167/CircularQueues_167.cpp
+++ CircularQueues_167/CircularQueues_167.cpp
@@ -112,6 +112,18 @@ int main() {
 			cin >> ch;
 			cout << endl;
 
+			switch (ch)
+			{
+			case '1': {
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+			}
+			
+			}
+
 		}
 	}
 }
